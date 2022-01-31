1a99ad58bf686060d6cf89c3cc1da709e8c951e6
Vijay <vjwarboy13@gmail.com>
2022-01-31 08:21:09 +0530

    Lightbox added

---
+++ src/components/LightBox.js
@@ -0,0 +1,172 @@
+import React, { useState, useEffect } from "react";
+import { CircularProgress, Modal, IconButton } from "@mui/material";
+import { createStyles, makeStyles } from "@mui/styles";
+import CloseIcon from "@mui/icons-material/Close";
+import "./index.css";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+      flexWrap: "wrap",
+      justifyContent: "space-around",
+      overflow: "hidden",
+    },
+    gridList: {
+      width: "100%",
+      height: "auto",
+    },
+    icon: {
+      color: "rgba(255, 255, 255, 0.54)",
+    },
+    mediaListTile: {
+      height: "auto",
+      padding: "6px 4px",
+    },
+    button: {
+      color: "black",
+      border: "2px solid black",
+      backgroundColor: "white",
+      fontSize: "2rem",
+    },
+    lightbox: {
+      height: "80vh",
+      maxWidth: "90vw",
+      objectFit: "cover",
+    },
+    arrowButton: {
+      color: "#ffffff9e",
+      "&:hover": {
+        backgroundColor: "#808080",
+      },
+      backgroundColor: "#696969",
+    },
+  })
+);
+
+export default function LightBox(props) {
+  const classes = useStyles();
+  const { mediaItems, callback } = props;
+  const mediaItems1 = [];
+  const [toggler, setToggler] = useState(true);
+  const [currentSlide, setCurrentSlide] = useState(props.currentSlide);
+  const [media, setMedia] = useState("");
+  mediaItems?.map((mediaData, index) => {
+    if (mediaData.type === "IMAGE" && mediaData.media) {
+      mediaItems1.push({
+        url: mediaData.media,
+        type: mediaData.type,
+      });
+    }
+    if (mediaData.type === "VIDEO" && mediaData.media) {
+      mediaItems1.push({
+        url: mediaData.media,
+        type: mediaData.type,
+      });
+    }
+    if (mediaData.type === "AUDIO" && mediaData.media) {
+      mediaItems1.push({
+        url: mediaData.media,
+        type: mediaData.type,
+      });
+    }
+  });
+  useEffect(() => {
+    setMedia(mediaItems1[currentSlide].url);
+  });
+
+  const toggleIsOpen = () => {
+    setToggler(!toggler);
+    callback();
+  };
+
+  const showPrev = (e) => {
+    e.stopPropagation();
+    const currentIndex = currentSlide;
+    if (currentIndex <= 0) {
+      setCurrentSlide(mediaItems1.length - 1);
+    } else {
+      setCurrentSlide(currentIndex - 1);
+    }
+  };
+  const showNext = (e) => {
+    e.stopPropagation();
+    const currentIndex = currentSlide;
+    if (currentIndex >= mediaItems1.length - 1) {
+      setCurrentSlide(0);
+    } else {
+      setCurrentSlide(currentIndex + 1);
+    }
+  };
+
+  return (
+    <>
+      {toggler ? (
+        <Modal
+          open={toggler}
+          onClose={toggleIsOpen}
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+        >
+          <div className="lsbox">
+            <div className="lightbox-header">
+              <div className="item-count">
+                <h4 style={{ color: "#ffffff9e" }}>
+                  {currentSlide + 1}/{mediaItems1.length}
+                </h4>
+              </div>
+              <div className="close-btn">
+                <IconButton onClick={toggleIsOpen} className={classes.arrowButton} size="small">
+                  <CloseIcon />
+                </IconButton>
+              </div>
+            </div>
+            <div className="left">
+              <IconButton onClick={showPrev} className={classes.arrowButton} size="small">
+                <ArrowBackIcon />
+              </IconButton>
+            </div>
+            {media ? (
+              <div style={{ display: "contents" }}>
+                {mediaItems1[currentSlide].type === "IMAGE" ? (
+                  <>
+                    <img src={media} className={classes.lightbox} />
+                  </>
+                ) : mediaItems1[currentSlide].type === "VIDEO" ? (
+                  <div>
+                    <video className={classes.lightbox} controls>
+                      <source src={media} type="video/mp4" />
+                      <source src={media} type="video/ogg" />
+                    </video>
+                  </div>
+                ) : mediaItems1[currentSlide].type === "AUDIO" ? (
+                  <div
+                    className="audio-player"
+                    style={{
+                      alignItems: "center",
+                      justifyContent: "center",
+                      width: "30%",
+                      height: "auto",
+                    }}
+                  >
+                    <audio src={media} controls autoPlay />
+                  </div>
+                ) : null}
+              </div>
+            ) : (
+              <div style={{ alignItems: "center", justifyContent: "center" }}>
+                <CircularProgress size={80} />
+              </div>
+            )}
+            <div className="right">
+              <IconButton onClick={showNext} className={classes.arrowButton} size="small">
+                <ArrowForwardIcon />
+              </IconButton>
+            </div>
+          </div>
+        </Modal>
+      ) : null}
+    </>
+  );
+}
---
+++ src/components/index.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { Grid, Box, styled, Paper } from "@mui/material";
+import video1 from "./assets/city.mp4";
+import audio1 from "./assets/sample.mp3";
+import LightBox from "./LightBox";
+const image1 =
+  "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80";
+const image2 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrKHPsvNDJHY9tWpkHrfkfo8Dkf0LvZU3Hdg&usqp=CAU";
+const data = [
+  {
+    media: image1,
+    type: "IMAGE",
+  },
+  {
+    media: image2,
+    type: "IMAGE",
+  },
+  {
+    media: video1,
+    type: "VIDEO",
+  },
+  {
+    media: audio1,
+    type: "AUDIO",
+  },
+];
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+export default function Home() {
+  const [toggler, setToggler] = useState(false);
+  const [currentSlide, setCurrentSlide] = useState();
+  return (
+    <div>
+      <h3>Examples</h3>
+      <div>
+        <Box sx={{ flexGrow: 1, marginLeft: 10, marginRight: 10 }}>
+          {data ? (
+            data?.map((val, i) => (
+              <Grid container spacing={2} key={i}>
+                <Grid item xs={12} md={12}>
+                  <Item
+                    onClick={() => {
+                      setCurrentSlide(i);
+                      setToggler(true);
+                    }}
+                  >
+                    {val.type === "IMAGE" ? (
+                      <img src={val.media} height="300px" width="300px" />
+                    ) : val.type === "VIDEO" ? (
+                      <video width="320" height="240" controls>
+                        <source src={val.media} type="video/mp4" />
+                        <source src={val.media} type="video/ogg" />
+                      </video>
+                    ) : val.type === "AUDIO" ? (
+                      <audio controls>
+                        <source src={val.media} type="audio/ogg" />
+                        <source src={val.media} type="audio/mpeg" />
+                      </audio>
+                    ) : (
+                      <h2> This Media file is not Supported</h2>
+                    )}
+                  </Item>
+                </Grid>
+              </Grid>
+            ))
+          ) : (
+            <h3>No Data</h3>
+          )}
+        </Box>
+        {toggler ? (
+          <LightBox
+            currentSlide={currentSlide}
+            mediaItems={data}
+            toggler={toggler}
+            callback={() => {
+              setToggler(false);
+            }}
+          />
+        ) : (
+          ""
+        )}
+      </div>
+    </div>
+  );
+}
