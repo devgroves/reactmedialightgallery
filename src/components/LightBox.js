aebbab224bf1d4def0d5767eec38602ded9a074f
Vijay <vjwarboy13@gmail.com>
2022-03-06 15:09:00 +0530

    Download function added without any package

--- src/components/LightBox.js
+++ src/components/LightBox.js
@@ -147,7 +147,27 @@ export default function LightBox(props) {
     }
   };
   const downloadMedia = () => {
-    saveAs(media, `media`);
+    fetch(media).then(res => res.blob()).then(data => {
+      const blobUrl = URL.createObjectURL(data);
+      // Create a link element
+      const link = document.createElement("a");
+      // Set link's href to point to the Blob URL
+      link.href = blobUrl;
+      link.download = "media";
+      // Append link to the body
+      document.body.appendChild(link);
+      // Dispatch click event on the link
+      // This is necessary as link.click() does not work on the latest firefox
+      link.dispatchEvent(
+        new MouseEvent('click', {
+          bubbles: true,
+          cancelable: true,
+          view: window
+        })
+      );
+      // Remove link from body
+      document.body.removeChild(link);
+    })
   };
   const ZoomIn = () => {
     setScale(scale * 1.1);
