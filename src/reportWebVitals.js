1151bfe6fdb81a60d0d5ca0436627d7ca35dd5ff
nagappan <nagappan08@gmail.com>
2022-12-21 18:14:29 +0530

    Introducing invalid import for codacy

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,3 +1,5 @@
+import('test');
+
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
